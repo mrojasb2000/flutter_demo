5507774969c15b430fa7561cadd6a6c2c879af42
Mauricio Rojas <mrojasb2000@gmail.com>
2022-07-20 12:29:25 -0400

    feat: add basic demo project

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue[800],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Hi Jared!
+                    const Text("Hi Jared!",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        )),
+
+                    // Notification
+                    const Icon(Icons.notifications, color: Colors.white),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
